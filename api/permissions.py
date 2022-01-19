463ca208a9e22ab7a79cc34388d0cf1cdfa1a4c0
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-01-19 20:32:55 +0530

    added custom permission

---
+++ api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import IsAuthenticated
+
+class IsOwnerAndAuthenticated(IsAuthenticated):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
