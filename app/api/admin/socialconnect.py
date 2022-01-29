c6148d04a16db38019e02a8f4ff6940f075e2741
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-01-29 18:03:10 +0530

    adding extra fields to model

--- app/api/admin/socialconnect.py
+++ app/api/admin/socialconnect.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from api.models import SocialConnect
 
 class SocialConnectAdmin(admin.ModelAdmin):
-    readonly_fields = ('user', 'access_token', 'token_type', 'expiry_date',)
+    readonly_fields = ('social_userid', 'social_account_type', 'social_media_count', 'social_username', 
+        'user', 'social_access_token', 'social_token_type', 'social_expiry_date',)
     list_display = ('user',)
     list_filter = ('user',)
     fieldsets = (
-        ('User details', {'fields': ('user',)}),
-        ('Token details', {'fields': ('access_token', 'token_type', 'expiry_date',)}),
+        ('User details', {'fields': ('social_userid', 'social_account_type', 'social_media_count', 'social_username', 'user',)}),
+        ('Token details', {'fields': ('social_access_token', 'social_token_type', 'social_expiry_date',)}),
     )
     search_fields = ('user',)
     filter_horizontal = ()
--- app/api/models/socialconnect.py
+++ app/api/models/socialconnect.py
@@ -2,14 +2,18 @@ from django.db import models
 from api.models import User
 
 class SocialConnect(models.Model):
+    social_userid = models.CharField(max_length=100)
+    social_account_type = models.CharField(max_length=20)
+    social_media_count = models.IntegerField(default=0)
+    social_username = models.CharField(max_length=100)
     user = models.ForeignKey(
         'User',
         verbose_name='User',
         on_delete=models.CASCADE
     )
-    access_token = models.TextField()
-    expiry_date = models.DateTimeField()
-    token_type = models.CharField(max_length=10)
+    social_access_token = models.TextField()
+    social_expiry_date = models.DateTimeField()
+    social_token_type = models.CharField(max_length=10)
 
     def __str__(self):
         return str(self.user)
