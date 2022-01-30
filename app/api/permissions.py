73074ac777449d5cdd83de9d66d092b4855f12f3
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-01-30 19:26:10 +0530

    fixing permission

--- app/api/permissions.py
+++ app/api/permissions.py
@@ -5,4 +5,4 @@ class IsOwnerAndAuthenticated(IsAuthenticated):
     Object-level permission to only allow owners of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        return obj.user_id == request.user.userid
