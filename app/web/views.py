a44e9530e63d9442f54d1206f9f9b51daec1c9d1
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-02-03 19:03:24 +0530

    html setup with bootstrap 5.1.3

--- app/web/views.py
+++ app/web/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "index.html"
+    extra_context = {'title': 'Home Page'}
