e80bdd878c03b3394dd82e138f287399515e69c0
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-02-27 18:28:55 +0530

    login signup template added

--- app/web/urls.py
+++ app/web/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
-from web.views import IndexView
+from django.urls import path, include, reverse
+from web.views import IndexView, SignupView, LoginView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login')
 ]
