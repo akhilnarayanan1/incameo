f257e283abf5df6d3a84291d540f2195141faec3
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-04-08 21:49:13 +0530

    not sure how to scale DB, will think later

--- api-service/app/incameo/settings.py
+++ api-service/app/incameo/settings.py
@@ -85,18 +85,18 @@ PASSWORD_HASHERS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ['POSTGRES_DB'],
-       'USER': os.environ['POSTGRES_USER'],
-       'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-       'HOST':  os.environ.get('POSTGRES_HOST', 'db'),
-       'PORT':  os.environ.get('POSTGRES_PORT', '5432'),
-   }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+#     'production': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.environ['POSTGRES_DB'],
+#        'USER': os.environ['POSTGRES_USER'],
+#        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+#        'HOST':  os.environ.get('POSTGRES_HOST', 'db'),
+#        'PORT':  os.environ.get('POSTGRES_PORT', '5432'),
+#    }
 }
 
 # Password validation
