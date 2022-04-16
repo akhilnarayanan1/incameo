47930e1a6edbf6c2780b11fa09ae0343a4978db1
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-04-16 15:27:56 +0530

    NonFieldError and MessageResponse

--- api-service/app/api/viewsets/auth.py
+++ api-service/app/api/viewsets/auth.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import mixins, viewsets, permissions, status
-from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from api.functions import mask_email
@@ -12,6 +11,7 @@ from api.serializers import (
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.utils.timezone import now
+from api.custom_response import NonFieldError, MessageResponse
 
 User = get_user_model()
 
@@ -25,7 +25,7 @@ class CreateAccountViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
   def create(self, request, *args, **kwargs):
     response = super().create(request, *args, **kwargs)
-    return Response(["User created successfully"], status=status.HTTP_201_CREATED)
+    return MessageResponse("User created successfully", status=status.HTTP_201_CREATED)
 
 
 class VerifyViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin, 
@@ -41,23 +41,23 @@ class VerifyViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
     response = super().retrieve(request, *args, **kwargs)
     token = self.get_object()
     if token.token_type != 'verify':
-      raise ValidationError('Invalid Token')
+      raise NonFieldError(['Invalid Token'])
     if token.token_expiry < now() or token.is_used:
-      raise ValidationError('Token is expired or already used')
+      raise NonFieldError(['Token is expired or already used'])
     user = token.user
     if user.is_banned:
-      raise ValidationError('User is banned')
+      raise NonFieldError(['This account is banned'])
     if user.is_verified:
-      raise ValidationError('User already verified')
+      raise NonFieldError(['Account already verified'])
     user.is_verified = True
     user.save()
     token.is_used = True
     token.save()
-    return Response(["Account verified succesfully"], status=status.HTTP_200_OK)
+    return MessageResponse("Account verified succesfully", status=status.HTTP_200_OK)
 
   def create(self, request, *args, **kwargs):
     response = super().create(request, *args, **kwargs)
-    return Response(["Token created successfully"], status=status.HTTP_201_CREATED)
+    return MessageResponse("Token created successfully", status=status.HTTP_201_CREATED)
   
   def perform_create(self, serializer):
     return create_or_verify(serializer, 'verify')
@@ -77,18 +77,18 @@ class ForgotViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.U
     response = super().retrieve(request, *args, **kwargs)
     token = self.get_object()
     if token.token_type != 'forgot':
-      raise ValidationError('Invalid Token')
+      raise NonFieldError(['Invalid Token'])
     if token.token_expiry < now() or token.is_used:
-      raise ValidationError('Token is expired or already used')
+      raise NonFieldError(['Token is expired or already used'])
     user = token.user
-    masked_email = ""#mask_email(user.email)
+    masked_email = mask_email(user.email)
     if user.is_banned:
-      raise ValidationError('User is banned')
-    return Response([f"Token verified succesfully for {masked_email}"], status=status.HTTP_200_OK)
+      raise NonFieldError(['This account is banned'])
+    return MessageResponse(f"Token verified succesfully for {masked_email}", status=status.HTTP_200_OK)
 
   def create(self, request, *args, **kwargs):
     response = super().create(request, *args, **kwargs)
-    return Response([f"Token created successfully"], status=status.HTTP_201_CREATED)
+    return MessageResponse(f"Token created successfully", status=status.HTTP_201_CREATED)
   
   def perform_create(self, serializer):
     return create_or_verify(serializer, 'forgot')
@@ -99,7 +99,7 @@ class ForgotViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.U
     token.is_used = True
     token.save()
     masked_email = mask_email(self.get_object().user.email)
-    return Response([f"Password changed succesfully for {masked_email}"], status=status.HTTP_200_OK)
+    return MessageResponse(f"Password changed succesfully for {masked_email}", status=status.HTTP_200_OK)
 
   def perform_update(self, serializer):
     user = self.get_object().user
@@ -110,21 +110,21 @@ class ForgotViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.U
 
 def create_or_verify(serializer, token_type):
   if token_type != 'verify' and token_type != 'forgot':
-    raise ValidationError('Invalid token type')
+    raise NonFieldError(['Invalid token type'])
   user=serializer.validated_data['user']
   try:
     user = User.objects.get(email=user)
     if token_type == 'verify' and user.is_verified:
-      raise ValidationError('User already verified')
+      raise NonFieldError(['User already verified'])
     available_token = AllVerifyOrForgotToken.objects.get(
       user=user, 
       token_type=token_type, 
       token_expiry__gt=now(),
       is_used=False
     )
-    raise ValidationError('Token already exists')
+    raise NonFieldError(['Token already exists'])
   except User.DoesNotExist:
-    raise ValidationError('User doesn\'t exist')
+    raise NonFieldError(['User doesn\'t exist'])
   except AllVerifyOrForgotToken.DoesNotExist:
     serializer.save(user=user, token_type=token_type)
 
@@ -141,4 +141,4 @@ class EditProfileViewset(mixins.UpdateModelMixin, viewsets.GenericViewSet):
       user = self.get_object()
       user.set_password(password)
       user.save()
-    return Response(["Profile updated successfully"], status=status.HTTP_200_OK)
+    return MessageResponse("Profile updated successfully", status=status.HTTP_200_OK)
